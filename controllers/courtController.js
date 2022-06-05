0d6013053c57814799e0d9ad590d349ab10fda9d
yaara-rubin <yaara.rubin@gmail.com>
2022-06-05 11:53:30 +0300

    change mongodv atlas

--- controllers/courtController.js
+++ controllers/courtController.js
@@ -13,59 +13,59 @@ CourtRouter.get('/', async (req, res) => {
     }
 });
 
-// CourtRouter.get('/:id', async (req, res) => {
-//     let { id } = req.params;
+CourtRouter.get('/:id', async (req, res) => {
+    let { id } = req.params;
 
-//     try {
-//         let note = await new Court().GetNoteByID(id);
-//         if (note.title == undefined) 
-//             res.status(404).json({ message: 'note not found', note });
-//         else
-//             res.status(200).json(note);
-//     } catch (error) {
-//         res.status(500).json({ error });
-//     }
-// });
+    try {
+        let note = await new Court().GetNoteByID(id);
+        if (note.title == undefined) 
+            res.status(404).json({ message: 'note not found', note });
+        else
+            res.status(200).json(note);
+    } catch (error) {
+        res.status(500).json({ error });
+    }
+});
 
-// NoteRouter.post('/add', async (req, res) => {
-//     /*
-//      * setp 0: make sure to require the model class
-//      * step 1: get the data from the req.body 
-//      * step 2: create a ne instance of the class
-//      * step 3: connect to DB
-//      * step 4: insert the record
-//      */
-//     let { title, description } = req.body;
-//     let note = new Court(title, description);
+NoteRouter.post('/add', async (req, res) => {
+    /*
+     * setp 0: make sure to require the model class
+     * step 1: get the data from the req.body 
+     * step 2: create a ne instance of the class
+     * step 3: connect to DB
+     * step 4: insert the record
+     */
+    let { title, description } = req.body;
+    let note = new Court(title, description);
 
-//     try {
-//         let result = await note.InsertNewNote();
-//         res.status(201).json(result);
-//     } catch (error) {
-//         res.status(500).json({ error })
-//     }
-// });
+    try {
+        let result = await note.InsertNewNote();
+        res.status(201).json(result);
+    } catch (error) {
+        res.status(500).json({ error })
+    }
+});
 
-// NoteRouter.put('/:id', async (req, res) => {
-//     let {id} = req.params;
-//     let {title, description} = req.body;
-//     try {
-//         let result = await new Note(title, description).UpdateNoteById(id);
-//         res.status(200).json(result);
-//     } catch (error) {
-//         res.status(500).json({ error });
-//     }
-// });
+NoteRouter.put('/:id', async (req, res) => {
+    let {id} = req.params;
+    let {title, description} = req.body;
+    try {
+        let result = await new Note(title, description).UpdateNoteById(id);
+        res.status(200).json(result);
+    } catch (error) {
+        res.status(500).json({ error });
+    }
+});
 
-// NoteRouter.delete('/:id', async (req, res) => {
-//     let {id} = req.params;
-//     try {
-//         let result = await new Note().DeleteNote(id);
-//         res.status(200).json(result);
-//     } catch (error) {
-//         es.status(500).json({ error });
-//     }
-// });
+NoteRouter.delete('/:id', async (req, res) => {
+    let {id} = req.params;
+    try {
+        let result = await new Note().DeleteNote(id);
+        res.status(200).json(result);
+    } catch (error) {
+        es.status(500).json({ error });
+    }
+});
 
 
 module.exports = CourtRouter;
