4c622e1c69840efd934f6ed7d2d288d724236942
Sitar Hirshberg <sitar55h@gmail.com>
2022-06-21 16:00:35 +0300

    check

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,10 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello From Server with react
-        </p>
-       
-      </header>
+     <h1>WORKING</h1>
     </div>
   );
 }
