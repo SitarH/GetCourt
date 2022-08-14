70b0293efc6332a5e9a4e08e62fedbef460feb47
yaara-rubin <yaara.rubin@gmail.com>
2022-08-14 14:22:47 +0300

    merge :)

--- client/src/Pages/History.js
+++ client/src/Pages/History.js
@@ -1,25 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
-import Order from '../Components/Order';
+import Wrapper from '../Components/UI/Wrapper';
+import Card from '../Components/UI/Card';
+import Title from '../Components/UI/Title';
 
 function History() {
 
-  const userGames = useSelector(state => state.auth.loggedUser.gamesList)
+  const [games, SetGames] = useState([]);
+  const order = useSelector(state => state.auth.loggedUser.gamesList);
 
+  const GetAllGames = () => {
+    console.log('order', order)
+    SetGames(order)
+  }
 
-
+  useEffect(() => {
+    GetAllGames()
+  }, []);
 
 
   return (
     <>
-   {userGames.map((game, index) =>{
-      return <Order key={index}>
-      
-      </Order>
+      <Wrapper className="column">
+        <Card>
+          <Title>All Court Orders</Title>
+          {
+            games.map((game, index) => (
+              Object.entries(game).map(([key, val]) => (
+                <h2>{key}:{val}</h2>
+              ))
+            ))
+          }
 
-    })
-   }
-   </>
+        </Card>
+      </Wrapper>
+    </>
+
   )
 }
 
--- client/src/Pages/SavedCards.js
+++ client/src/Pages/SavedCards.js
@@ -1,8 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { useSelector } from 'react-redux';
+import Wrapper from '../Components/UI/Wrapper';
+import Card from '../Components/UI/Card';
+import Title from '../Components/UI/Title';
+
 
 function SavedCards() {
+
+  const currentUser = useSelector(state => state.auth.loggedUser)
+
   return (
-    <div>SavedCards</div>
+    <>
+    <Wrapper className="column">
+      <Card>
+        <Title>Saved Cards </Title>
+
+        <p>Card Number: {currentUser.cardNumber}</p>
+      </Card>
+    </Wrapper>
+
+    </>
   )
 }
 
